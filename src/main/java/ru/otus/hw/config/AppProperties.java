af9f7a887399a450ad0b47013e0ca80ef9fb9749
Shabdan <shabdanlp@gmail.com>
2024-02-08 23:55:26 +0300

    Displaying questions and answers from csv to console.

--- src/main/java/ru/otus/hw/config/AppProperties.java
+++ src/main/java/ru/otus/hw/config/AppProperties.java
@@ -2,7 +2,9 @@ package ru.otus.hw.config;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
 @AllArgsConstructor
 @Data
 public class AppProperties implements TestFileNameProvider {
--- src/main/java/ru/otus/hw/service/TestServiceImpl.java
+++ src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -1,16 +1,26 @@
 package ru.otus.hw.service;
 
 import lombok.RequiredArgsConstructor;
+import ru.otus.hw.dao.QuestionDao;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 public class TestServiceImpl implements TestService {
 
     private final IOService ioService;
 
+    private final QuestionDao csvQuestionDao;
+
     @Override
     public void executeTest() {
         ioService.printLine("");
         ioService.printFormattedLine("Please answer the questions below%n");
         // Получить вопросы из дао и вывести их с вариантами ответов
+        printList(csvQuestionDao.findAll());
+    }
+
+    private void printList(List<?> objectList) {
+        objectList.forEach(o -> System.out.println(o));
     }
 }
