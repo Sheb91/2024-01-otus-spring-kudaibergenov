43cbfb703384a16db4f99b9de066f448ad5766b0
Shabdan <shabdanlp@gmail.com>
2024-03-01 16:15:04 +0300

    Changes in CsvQuestionDao, TestServiceImpl for running app correctly. Added localization attributes.

--- hw3/src/main/java/ru/otus/hw/service/TestServiceImpl.java
+++ hw3/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -3,6 +3,7 @@ package ru.otus.hw.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.domain.Question;
 import ru.otus.hw.domain.Student;
 import ru.otus.hw.domain.TestResult;
 
@@ -24,10 +25,24 @@ public class TestServiceImpl implements TestService {
         var testResult = new TestResult(student);
 
         for (var question: questions) {
-            var isAnswerValid = false; // Задать вопрос, получить ответ
+            var isAnswerValid = isCorrectAnswer(question);
             testResult.applyAnswer(question, isAnswerValid);
         }
         return testResult;
     }
 
+    private boolean isCorrectAnswer(Question question) {
+        ioService.printFormattedLineLocalized("TestService.exact.question", question.text());
+        int rightAnswerIndex = -1;
+        for (int i = 0; i < question.answers().size(); i++) {
+            ioService.printFormattedLineLocalized("TestService.exact.answer", i + 1, question.answers().get(i).text());
+            if (question.answers().get(i).isCorrect()) {
+                rightAnswerIndex = i + 1;
+            }
+        }
+        int chosenAnswerToQuestion = ioService.readIntForRangeWithPromptLocalized(1, question.answers().size(),
+                "TestService.exact.user.answer",
+                "TestService.exact.user.mistake");
+        return rightAnswerIndex == chosenAnswerToQuestion;
+    }
 }
