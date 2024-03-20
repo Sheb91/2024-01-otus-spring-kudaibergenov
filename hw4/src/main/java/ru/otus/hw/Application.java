d90a20156e141c26a7bd76399b9dbf5155fa5170
Shabdan <shabdanlp@gmail.com>
2024-03-20 15:13:31 +0300

    Deleted unused imports.

--- hw4/src/main/java/ru/otus/hw/Application.java
+++ hw4/src/main/java/ru/otus/hw/Application.java
@@ -3,7 +3,6 @@ package ru.otus.hw;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.shell.command.annotation.CommandScan;
 import ru.otus.hw.config.AppProperties;
 
 @EnableConfigurationProperties(AppProperties.class)
